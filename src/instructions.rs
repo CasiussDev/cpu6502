24294d44ea2f065f5d48e10d6f5e97dcb84fa8ef
casiuss <slugserjee@gmail.com>
2022-09-12 22:52:42 +0200

    more instructions

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,7 +1,9 @@
-use crate::registers::register_file::SelectedRegister;
-use std::collections::HashMap;
 use crate::alu::{AluBinaryOp, AluUnaryOp};
+use crate::registers::register_file::SelectedRegister;
 use crate::registers::StatusRegFlags;
+use std::collections::HashMap;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
 #[allow(dead_code)]
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
@@ -52,16 +54,31 @@ pub enum MicroInstruction {
 }
 
 #[allow(dead_code)]
-#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+#[derive(EnumIter, Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub enum InstructionOp {
-    INX,
-    INY,
-    DEX,
-    DEY,
+    Nop,
+    IncrementX,
+    IncrementY,
+    DecrementX,
+    DecrementY,
+    ClearCarry,
+    SetCarry,
+    ClearDecimal,
+    SetDecimal,
+    ClearInterruptDisable,
+    SetInterruptDisable,
+    ClearOverflow,
+    SetOverflow,
+    TransferAccumulatorToX,
+    TransferAccumulatorToY,
+    TransferStackPtrToX,
+    TransferXToAccumulator,
+    TransferYToAccumulator,
+    TransferXToStackPtr,
 }
 
 #[allow(dead_code)]
-#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+#[derive(EnumIter, Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub enum InstructionSequenceMode {
     Break,
     ReturnInterrupt,
@@ -384,31 +401,125 @@ type OpsMap = std::collections::HashMap<InstructionOp, MicroInstructionsVector>;
 pub fn create_instructionops_sequences() -> OpsMap {
     let ops = HashMap::from([
         (
-            InstructionOp::INX,
+            InstructionOp::Nop,
+            vec![],
+        ),
+        (
+            InstructionOp::IncrementX,
             vec![MicroInstruction::AluUnaryOp {
                 op: AluUnaryOp::Inc,
                 reg: SelectedRegister::X,
             }],
         ),
         (
-            InstructionOp::INY,
+            InstructionOp::IncrementY,
             vec![MicroInstruction::AluUnaryOp {
                 op: AluUnaryOp::Inc,
                 reg: SelectedRegister::Y,
             }],
         ),
         (
-            InstructionOp::DEX,
+            InstructionOp::DecrementX,
             vec![MicroInstruction::AluUnaryOp {
                 op: AluUnaryOp::Dec,
                 reg: SelectedRegister::X,
             }],
         ),
         (
-            InstructionOp::DEY,
+            InstructionOp::DecrementY,
             vec![MicroInstruction::AluUnaryOp {
                 op: AluUnaryOp::Dec,
                 reg: SelectedRegister::Y,
+            }],
+        ),
+        (
+            InstructionOp::ClearCarry,
+            vec![MicroInstruction::ClearStatusFlag {
+                flag: StatusRegFlags::CARRY,
+            }],
+        ),
+        (
+            InstructionOp::SetCarry,
+            vec![MicroInstruction::SetStatusFlag {
+                flag: StatusRegFlags::CARRY,
+            }],
+        ),
+        (
+            InstructionOp::ClearDecimal,
+            vec![MicroInstruction::ClearStatusFlag {
+                flag: StatusRegFlags::DECIMAL,
+            }],
+        ),
+        (
+            InstructionOp::SetDecimal,
+            vec![MicroInstruction::SetStatusFlag {
+                flag: StatusRegFlags::DECIMAL,
+            }],
+        ),
+        (
+            InstructionOp::ClearInterruptDisable,
+            vec![MicroInstruction::ClearStatusFlag {
+                flag: StatusRegFlags::IRQ_DISABLE,
+            }],
+        ),
+        (
+            InstructionOp::SetInterruptDisable,
+            vec![MicroInstruction::SetStatusFlag {
+                flag: StatusRegFlags::IRQ_DISABLE,
+            }],
+        ),
+        (
+            InstructionOp::ClearOverflow,
+            vec![MicroInstruction::ClearStatusFlag {
+                flag: StatusRegFlags::OVERFLOW,
+            }],
+        ),
+        (
+            InstructionOp::SetOverflow,
+            vec![MicroInstruction::SetStatusFlag {
+                flag: StatusRegFlags::OVERFLOW,
+            }],
+        ),
+        (
+            InstructionOp::TransferAccumulatorToX,
+            vec![MicroInstruction::CopyRegister {
+                src: SelectedRegister::A,
+                dst: SelectedRegister::X,
+            }],
+        ),
+        (
+            InstructionOp::TransferAccumulatorToY,
+            vec![MicroInstruction::CopyRegister {
+                src: SelectedRegister::A,
+                dst: SelectedRegister::Y,
+            }],
+        ),
+        (
+            InstructionOp::TransferStackPtrToX,
+            vec![MicroInstruction::CopyRegister {
+                src: SelectedRegister::SP,
+                dst: SelectedRegister::X,
+            }],
+        ),
+        (
+            InstructionOp::TransferXToAccumulator,
+            vec![MicroInstruction::CopyRegister {
+                src: SelectedRegister::X,
+                dst: SelectedRegister::A,
+            }],
+        ),
+        (
+            InstructionOp::TransferYToAccumulator,
+            vec![MicroInstruction::CopyRegister {
+                src: SelectedRegister::Y,
+                dst: SelectedRegister::A,
+            }],
+        ),
+        (
+            InstructionOp::TransferXToStackPtr,
+            vec![MicroInstruction::CopyRegister {
+                src: SelectedRegister::X,
+                dst: SelectedRegister::SP,
             }],
         ),
     ]);
@@ -433,6 +544,30 @@ mod tests {
                 *last_microinstruction == MicroInstruction::YieldClock,
                 "Sequence mode {:?} does not finish with YieldClock",
                 mode
+            );
+        }
+    }
+
+    #[test]
+    fn check_all_instruction_modes_implemented() {
+        let sequence_modes = create_instruction_mode_sequences();
+        for mode in InstructionSequenceMode::iter() {
+            assert!(
+                sequence_modes.contains_key(&mode),
+                "Mode {:?} not implemented",
+                mode,
+            )
+        }
+    }
+
+    #[test]
+    fn check_all_ops_implemented() {
+        let instruction_ops = create_instructionops_sequences();
+        for op in InstructionOp::iter() {
+            assert!(
+                instruction_ops.contains_key(&op),
+                "Operation {:?} not implemented",
+                op,
             );
         }
     }
