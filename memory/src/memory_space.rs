d2cadbd76570ca1d497df057f4032734d928843a
casiuss <slugserjee@gmail.com>
2023-08-14 18:56:56 +0200

    update cge_nes for memory access refactor

--- memory/src/memory_space.rs
+++ memory/src/memory_space.rs
@@ -1,12 +1,12 @@
 pub trait MemorySpace {
-    fn read(&self, addr: u16) -> u8;
+    fn read(&mut self, addr: u16) -> u8;
     fn write(&mut self, data: u8, addr: u16);
 }
 
 const MEMORY_64K: usize = (u16::MAX as usize) + 1;
 
 impl MemorySpace for [u8; MEMORY_64K] {
-    fn read(&self, addr: u16) -> u8 {
+    fn read(&mut self, addr: u16) -> u8 {
         self[addr as usize]
     }
 
