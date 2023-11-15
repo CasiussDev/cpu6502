40460649a9c974e26120b39fb393abd3eff7a892
casiuss <slugserjee@gmail.com>
2023-11-15 10:36:07 +0100

    show memory accesses in debug log

--- src/cpu/logging_memory.rs
+++ src/cpu/logging_memory.rs
@@ -1,5 +1,5 @@
 use crate::MemorySpace;
-use log::trace;
+use log::debug;
 
 pub struct LoggingMemory<'a, T> {
     inner: &'a mut T,
@@ -12,6 +12,10 @@ where
     pub fn new(inner: &'a mut T) -> Self {
         Self { inner }
     }
+
+    fn read_inner(&mut self, addr: u16) -> u8 {
+        self.inner.read(addr)
+    }
 }
 
 impl<'a, T> MemorySpace for LoggingMemory<'a, T>
@@ -20,12 +24,16 @@ where
 {
     fn read(&mut self, addr: u16) -> u8 {
         let data = self.inner.read(addr);
-        trace!("\t\t\tRead Memory[{:04X}] = {:02X}", addr, data);
+        debug!("\t\t\tRead Memory[{:04X}] = {:02X}", addr, data);
         data
     }
 
     fn write(&mut self, data: u8, addr: u16) {
-        trace!("\t\t\tWrite Memory[{:04X}] = {:02X}", addr, data,);
+        debug!("\t\t\tWrite Memory[{:04X}] = {:02X}", addr, data,);
         self.inner.write(data, addr);
+    }
+
+    fn read_array(&mut self, addr: u16, out: &mut [u8]) {
+        self.inner.read_array(addr, out);
     }
 }
