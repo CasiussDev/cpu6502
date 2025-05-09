4f445f63448345ff8755c1d360b63cf149210129
casiuss <slugserjee@gmail.com>
2025-05-09 23:37:39 +0200

    perf test

--- tests/perf_test.rs
+++ tests/perf_test.rs
@@ -1,22 +1,26 @@
+#[cfg(not(debug_assertions))]
 mod test_computer;
 
-use crate::test_computer::TestComputer;
+#[cfg(not(debug_assertions))]
+mod inner {
+    use super::test_computer::TestComputer;
 
-const NES_CLOCKS_SECOND: u128 = 1789773;
+    const NES_CLOCKS_SECOND: u128 = 1789773;
 
-fn run(computer: &mut TestComputer, num_cycles: u128) {
-    computer.cpu.reset();
+    fn run(computer: &mut TestComputer, num_cycles: u128) {
+        computer.cpu.reset();
 
-    while computer.cpu.cycle_count_since_reset() < num_cycles {
-        computer.cpu.run(&mut computer.memory);
+        while computer.cpu.cycle_count_since_reset() < num_cycles {
+            computer.cpu.run(&mut computer.memory);
+        }
     }
-}
 
-#[test]
-//#[ignore]
-fn perf_test() {
-    let mut computer = TestComputer::new();
-    computer.load_rom();
+    #[test]
+    #[cfg(not(debug_assertions))]
+    fn perf_test() {
+        let mut computer = TestComputer::new();
+        computer.load_rom();
 
-    run(&mut computer, 180 * NES_CLOCKS_SECOND);
+        run(&mut computer, 180 * NES_CLOCKS_SECOND);
+    }
 }
