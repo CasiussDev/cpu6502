6490387abd92c8d4c4569965c85c685f05326389
casiuss <slugserjee@gmail.com>
2025-05-15 00:45:06 +0200

    inner module for nes_test.rs

--- tests/nes_test.rs
+++ tests/nes_test.rs
@@ -1,107 +1,103 @@
-#[cfg(feature = "disassembly")]
-extern crate disasm6502;
-
-#[cfg(feature = "logging")]
-extern crate simplelog;
-
 mod test_computer;
 
 #[cfg(feature = "disassembly")]
-use crate::test_computer::TestComputer;
-#[cfg(feature = "disassembly")]
-use std::io::{BufRead, Write};
-#[cfg(feature = "disassembly")]
-use std::{fs, io};
-#[cfg(feature = "disassembly")]
-const REFERENCE_FILE: &'static str = "testdata/reference.6502log";
-#[cfg(feature = "disassembly")]
-const OUTPUT_FILE: &'static str = "testdata/output.6502log";
+mod inner {
+    use crate::test_computer::TestComputer;
+    use std::io::{BufRead, Write};
+    use std::{fs, io};
 
-#[cfg(feature = "disassembly")]
-fn run(computer: &mut TestComputer, num_cycles: u128) {
-    let log_file = fs::File::create(OUTPUT_FILE).expect("cannot open output log file");
+    extern crate disasm6502;
 
-    let mut log_file = io::BufWriter::new(log_file);
+    #[cfg(feature = "logging")]
+    extern crate simplelog;
 
-    let mut instr_log;
+    const REFERENCE_FILE: &'static str = "testdata/reference.6502log";
+    const OUTPUT_FILE: &'static str = "testdata/output.6502log";
 
-    computer.cpu.reset();
+    fn run(computer: &mut TestComputer, num_cycles: u128) {
+        let log_file = fs::File::create(OUTPUT_FILE).expect("cannot open output log file");
 
-    while computer.cpu.cycle_count_since_reset() < num_cycles {
-        computer.cpu.run(&mut computer.memory);
+        let mut log_file = io::BufWriter::new(log_file);
 
-        if let Some(addr) = computer.cpu.fetched_instr_addr() {
-            let instructions = disasm6502::from_addr_array(
-                &computer.memory[(addr as usize)..(addr as usize + 6)],
-                addr,
-            )
-            .expect("could not decode instr");
-            let decoded = instructions.first().expect("empty instr vector");
+        let mut instr_log;
 
-            instr_log = format!("{:04X} {} {}", addr, decoded.as_hex_str(), decoded.as_str());
+        computer.cpu.reset();
 
-            writeln!(
-                log_file,
-                "{:<25}{} CYC:{}",
-                instr_log,
-                computer.cpu.regs_as_log_line(),
-                computer.cpu.cycle_count_since_reset()
-            )
-            .expect("cannot write to file");
+        while computer.cpu.cycle_count_since_reset() < num_cycles {
+            computer.cpu.run(&mut computer.memory);
+
+            if let Some(addr) = computer.cpu.fetched_instr_addr() {
+                let instructions = disasm6502::from_addr_array(
+                    &computer.memory[(addr as usize)..(addr as usize + 6)],
+                    addr,
+                )
+                .expect("could not decode instr");
+                let decoded = instructions.first().expect("empty instr vector");
+
+                instr_log = format!("{:04X} {} {}", addr, decoded.as_hex_str(), decoded.as_str());
+
+                writeln!(
+                    log_file,
+                    "{:<25}{} CYC:{}",
+                    instr_log,
+                    computer.cpu.regs_as_log_line(),
+                    computer.cpu.cycle_count_since_reset()
+                )
+                .expect("cannot write to file");
+            }
+        }
+
+        log_file.flush().expect("could not flush file writer");
+    }
+
+    fn check_results() {
+        let reference_file_len = fs::metadata(REFERENCE_FILE)
+            .expect("could not read metadata of {REFERENCE_FILE}")
+            .len();
+        let output_file_len = fs::metadata(OUTPUT_FILE)
+            .expect("could not read metadata of {OUTPUT_FILE}")
+            .len();
+
+        assert_eq!(output_file_len, reference_file_len);
+
+        let reference_file =
+            fs::File::open(REFERENCE_FILE).expect("could not open {REFERENCE_FILE}");
+        let output_file = fs::File::open(OUTPUT_FILE).expect("could not open {OUTPUT_FILE}");
+
+        let reference_lines = io::BufReader::new(reference_file).lines();
+        let output_lines = io::BufReader::new(output_file).lines();
+
+        for (reference, output) in reference_lines.zip(output_lines) {
+            assert_eq!(
+                reference.expect("cannot unwrap reference trace line"),
+                output.expect("cannot unwrap output trace line")
+            );
         }
     }
 
-    log_file.flush().expect("could not flush file writer");
-}
+    #[test]
+    fn nes_rom_test() {
+        #[cfg(feature = "logging")]
+        {
+            let log_config = simplelog::ConfigBuilder::new()
+                .set_max_level(log::LevelFilter::Off)
+                .set_time_level(log::LevelFilter::Off)
+                .set_thread_level(log::LevelFilter::Off)
+                .set_target_level(log::LevelFilter::Off)
+                .set_location_level(log::LevelFilter::Off)
+                .build();
 
-#[cfg(feature = "disassembly")]
-fn check_results() {
-    let reference_file_len = fs::metadata(REFERENCE_FILE)
-        .expect("could not read metadata of {REFERENCE_FILE}")
-        .len();
-    let output_file_len = fs::metadata(OUTPUT_FILE)
-        .expect("could not read metadata of {OUTPUT_FILE}")
-        .len();
+            let trace_file =
+                fs::File::create("testdata/trace.log.txt").expect("cannot open trace file");
+            simplelog::WriteLogger::init(log::LevelFilter::Trace, log_config, trace_file)
+                .expect("WriteLogger::init error");
+        }
 
-    assert_eq!(output_file_len, reference_file_len);
+        let mut computer = TestComputer::new();
+        computer.load_rom();
 
-    let reference_file = fs::File::open(REFERENCE_FILE).expect("could not open {REFERENCE_FILE}");
-    let output_file = fs::File::open(OUTPUT_FILE).expect("could not open {OUTPUT_FILE}");
+        run(&mut computer, 14575);
 
-    let reference_lines = io::BufReader::new(reference_file).lines();
-    let output_lines = io::BufReader::new(output_file).lines();
-
-    for (reference, output) in reference_lines.zip(output_lines) {
-        assert_eq!(
-            reference.expect("cannot unwrap reference trace line"),
-            output.expect("cannot unwrap output trace line")
-        );
+        check_results();
     }
-}
-
-#[test]
-#[cfg(feature = "disassembly")]
-fn nes_rom_test() {
-    #[cfg(feature = "logging")]
-    {
-        let log_config = simplelog::ConfigBuilder::new()
-            .set_max_level(log::LevelFilter::Off)
-            .set_time_level(log::LevelFilter::Off)
-            .set_thread_level(log::LevelFilter::Off)
-            .set_target_level(log::LevelFilter::Off)
-            .set_location_level(log::LevelFilter::Off)
-            .build();
-
-        let trace_file =
-            fs::File::create("testdata/trace.log.txt").expect("cannot open trace file");
-        simplelog::WriteLogger::init(log::LevelFilter::Trace, log_config, trace_file)
-            .expect("WriteLogger::init error");
-    }
-
-    let mut computer = TestComputer::new();
-    computer.load_rom();
-
-    run(&mut computer, 14575);
-
-    check_results();
 }
